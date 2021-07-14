b89af7fd29a8d41a968e58bbb8f50e6d5e1d7a9c
Tai Chef <taidang@tamdongtam.vn>
2021-07-14 19:08:05 +0700

    init chef fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {UserModel} from "./user.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
   title = 'tai-chef';
+  data: UserModel[] | undefined;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllUser(): Observable<UserModel[]> {
+    return this.http.get<UserModel[]>("http://10.1.20.16:9999/api/users")
+  }
+
+  ngOnChanges() {
+  }
+
+  ngOnInit() {
+    this.getAllUser().subscribe((next) => {
+      if (next)
+        this.data = next
+    })
+  }
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,10 @@
+export interface UserModel{
+  userId: number,
+  username: string,
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string,
+  phone: string,
+  status: number
+}
